f9a99ad26f208f1989c693fe7b2ccd716b7caff9
Mohsen <m.pourali1982@gmail.com>
2021-12-01 02:25:42 +0330

    Basket developed

---
+++ src/Services/Basket/Basket.API/Repositoies/IBasketRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Basket.API.Entities;
+
+namespace Basket.API.Repositoies
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
